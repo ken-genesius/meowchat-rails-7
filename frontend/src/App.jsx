c4e64b88f38715ddb4ed66bb302b5f606dde337d
ken-genesius <iyus@LAPTOP-A7KK9GPI.>
2025-03-14 08:04:27 +0700

    style(login): login & register template

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,29 @@
 import Chat from "./components/chat/Chat";
 import Detail from "./components/detail/Detail";
 import List from "./components/list/List";
+import Login from "./components/login/Login";
+import Notification from "./components/notification/Notification";
+import { useUserStore } from "./lib/userStore";
 
 const App = () => {
+
+  const {currentUser, isLoading} = useUserStore();
+
+  if(isLoading) return <div className="loading">Loading...</div>
+  
   return (
     <div className='container'>
-      <List/>
-      <Chat/>
-      <Detail/>
+      {
+        currentUser ? (
+          <>
+            <List/>
+            <Chat/>
+            <Detail/>
+          </>
+        ) : (
+        <Login/>
+      )}
+      <Notification/>
     </div>
   )
 }
